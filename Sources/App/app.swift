e533a43bd967b2a75f214842fbb0fb6a9cd119e8
Eugene Antropov <e@antropov.it>
2018-11-23 23:23:18 +0300

    first commit

--- Sources/App/app.swift
+++ Sources/App/app.swift
@@ -1,4 +1,7 @@
 import Vapor
+import Dispatch
+
+let timer = DispatchSource.makeTimerSource()
 
 /// Creates an instance of Application. This is called from main.swift in the run target.
 public func app(_ env: Environment) throws -> Application {
@@ -8,5 +11,16 @@ public func app(_ env: Environment) throws -> Application {
     try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
+    registerTimer()
     return app
 }
+
+func registerTimer() {
+    
+    timer.schedule(deadline: .now() + .seconds(60), repeating: .seconds(60), leeway: .seconds(30))
+    timer.setEventHandler() {
+        print("Timer fired")
+    }
+    timer.resume()
+}
+
